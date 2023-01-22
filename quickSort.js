717eb69f003debeb496bdc7423a3c3d54cba64e6
Oscar Liu <44384935+IAmOscar-Liu@users.noreply.github.com>
2023-01-23 03:34:45 +0800

    Update quickSort.js

--- quickSort.js
+++ quickSort.js
@@ -1,31 +1,31 @@
-const swap = (arr, l, r) => {
-    const tmp = arr[l];
-    arr[l] = arr[r];
-    arr[r] = tmp;
-}
-
-const partition = (arr, left, right) => {
-    let l = left, j = right;
-
-    while (l < j) {
-        do { l ++ } while (l > left && arr[l] <= arr[left])
-        do { j -- } while (j < right && arr[j] > arr[left])
-
-        if (l < j) swap(arr, l, j);
-    }
-    swap(arr, left, j);
-    return j;
-}
-
-const doQuickSort = (arr, left, right) => {
-    if (left < right) {
-        const j = partition(arr, left, right);
-        doQuickSort(arr, left, j);
-        doQuickSort(arr, j + 1, right);
-    }
-}
-
 const quickSort = (arr) => {
+    const swap = (arr, l, r) => {
+        const tmp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = tmp;
+    }
+    
+    const partition = (arr, left, right) => {
+        let l = left, j = right;
+    
+        while (l < j) {
+            do { l ++ } while (l > left && arr[l] <= arr[left])
+            do { j -- } while (j < right && arr[j] > arr[left])
+    
+            if (l < j) swap(arr, l, j);
+        }
+        swap(arr, left, j);
+        return j;
+    }
+    
+    const doQuickSort = (arr, left, right) => {
+        if (left < right) {
+            const j = partition(arr, left, right);
+            doQuickSort(arr, left, j);
+            doQuickSort(arr, j + 1, right);
+        }
+    }
+    
     doQuickSort(arr, 0, arr.length);
     return arr;
 }
